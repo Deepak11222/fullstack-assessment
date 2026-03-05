5a5f468c4f9caeee7bea1e1029b66a8855d31d2a
Deepak11222 <ds9162659@gmail.com>
2026-03-05 12:39:13 +0530

    Setup React HRMS frontend structure

---
+++ client/src/pages/Attendance.jsx
@@ -0,0 +1,10 @@
+function Attendance() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Attendance Management</h1>
+      <p>Mark employee attendance</p>
+    </div>
+  );
+}
+
+export default Attendance;
---
+++ client/src/pages/Employees.jsx
@@ -0,0 +1,10 @@
+function Employees() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>HRMS Lite - Employees</h1>
+      <p>Employee management page</p>
+    </div>
+  );
+}
+
+export default Employees;
